fefee6fb9c4cf319dc9776dccf35e3a484741467
eflores <eflores@switchfly.com>
2015-02-17 07:31:19 +0000

    Add files for first practice

---
+++ practices/java-io/src/main/java/com/uabc/dowa/io/FileCharacterCounter.java
@@ -0,0 +1,7 @@
+package com.uabc.dowa.io;
+
+public class FileCharacterCounter {
+    public static int countNumberOfOccurrencesInFile(char character) {
+        return 0;
+    }
+}
---
+++ practices/java-io/src/test/java/com/uabc/dowa/io/FileCharacterCounterTest.java
@@ -0,0 +1,23 @@
+package com.uabc.dowa.io;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileCharacterCounterTest {
+    @Test
+    public void testCountNumberOfCharacterInFile() throws Exception {
+        assertEquals(0, FileCharacterCounter.countNumberOfOccurrencesInFile('x'));
+        assertEquals(0, FileCharacterCounter.countNumberOfOccurrencesInFile('z'));
+
+        assertEquals(0, FileCharacterCounter.countNumberOfOccurrencesInFile('2'));
+
+        assertEquals(8, FileCharacterCounter.countNumberOfOccurrencesInFile(','));
+        assertEquals(9, FileCharacterCounter.countNumberOfOccurrencesInFile('u'));
+        assertEquals(31, FileCharacterCounter.countNumberOfOccurrencesInFile('a'));
+        assertEquals(36, FileCharacterCounter.countNumberOfOccurrencesInFile('o'));
+
+        assertEquals(52, FileCharacterCounter.countNumberOfOccurrencesInFile('e'));
+        assertEquals(52, FileCharacterCounter.countNumberOfOccurrencesInFile('i'));
+    }
+}
