d55e995b3ae1c327f334c3229c0b03038b4523ac
Jesus Andres Morales Ramos <andresmor93@msn.com>
2015-03-08 15:23:11 -0700

    First version of the practice 2. I have some questions about some of the functions in the practice.

--- practices/java-misc-exercises/test/java/com/uabc/dowa/io/exercises/CharacterServiceTest.java
+++ practices/java-misc-exercises/test/java/com/uabc/dowa/io/exercises/CharacterServiceTest.java
@@ -1,5 +1,35 @@
 package com.uabc.dowa.io.exercises;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class CharacterServiceTest {
 
+    @Test
+    public void testAreAnagrams() {
+        CharacterServiceImpl chService = new CharacterServiceImpl();
+        assertEquals(true, chService.areAnagrams("animal", "lamina"));
+    }
+
+    @Test
+    public void testReverse() {
+        CharacterServiceImpl chService = new CharacterServiceImpl();
+        String s = "hola";
+        String s2 = "aloh";
+        char [] charArr = s.toCharArray();
+        chService.reverse(charArr);
+        assertArrayEquals(s2.toCharArray(), charArr);
+    }
+
+    @Test
+    public void testReverseWords() {
+        CharacterServiceImpl chService = new CharacterServiceImpl();
+        String s = "hola como estas";
+        String s2 = "estas como hola";
+        char [] charArr = s.toCharArray();
+        chService.reverseWords(charArr);
+        assertArrayEquals(s2.toCharArray(), charArr);
+    }
 }
--- practices/java-misc-exercises/test/java/com/uabc/dowa/io/exercises/NumberServiceTest.java
+++ practices/java-misc-exercises/test/java/com/uabc/dowa/io/exercises/NumberServiceTest.java
@@ -1,4 +1,68 @@
 package com.uabc.dowa.io.exercises;
 
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class NumberServiceTest {
+    @Test
+    public void testFindSingleMissingNumber(){
+        NumberServiceImpl classTest = new NumberServiceImpl();
+        int [] test = new int[100];
+        for(int i = 1; i <= 100; i++){
+            test[i-1] = i;
+        }
+        int index = this.randInt();
+        assertEquals(index, classTest.findSingleMissingNumber(removeElement(test, index)));
+    }
+
+    @Test
+    public void testFindTwoMissingNumbers(){
+        NumberServiceImpl classTest = new NumberServiceImpl();
+        int [] test = new int[100];
+        for(int i = 1; i <= 100; i++){
+            test[i-1] = i;
+        }
+        test = removeElement(test, 45);
+        test = removeElement(test,39);
+        int [] values = {45, 39};
+        assertArrayEquals(values, classTest.findTwoMissingNumbers(test));
+    }
+
+    @Test
+    public void testFindMissingNumbers(){
+
+    }
+
+    @Test
+    public void testFindNumberInSortedArray(){
+
+    }
+
+    public int randInt() {
+        int min = 1;
+        int max = 100;
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
+
+    public int[] removeElement(int[] arr, int del){
+        int i , j ;
+        for (i = j = 0; j < arr.length; ++j) {
+            if (j != del - 1) {
+                arr[i++] = arr[j];
+            }
+        }
+        return Arrays.copyOf(arr, i);
+    }
 }
