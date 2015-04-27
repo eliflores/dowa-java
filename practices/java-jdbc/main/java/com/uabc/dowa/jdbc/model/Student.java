e63439dbd839d8fad488f6e8264518cba56e6455
miriamRM <tsuki4u@gmail.com>
2015-04-27 14:22:40 -0700

    getting the last practice from dowa-java

--- practices/java-jdbc/main/java/com/uabc/dowa/jdbc/model/Student.java
+++ practices/java-jdbc/main/java/com/uabc/dowa/jdbc/model/Student.java
@@ -6,22 +6,27 @@ public class Student {
     private String _lastName;
 
     public int getId() {
+
         return _id;
     }
 
     public String getFirstName() {
+
         return _firstName;
     }
 
     public void setFirstName(String _firstName) {
+
         this._firstName = _firstName;
     }
 
     public String getLastName() {
+
         return _lastName;
     }
 
     public void setLastName(String _lastName) {
+
         this._lastName = _lastName;
     }
 }
