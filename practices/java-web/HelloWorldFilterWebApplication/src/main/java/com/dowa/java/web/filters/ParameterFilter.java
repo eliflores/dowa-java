c89b1b4a1b0cc027c594ce1e1f05764c8d07d773
miriamRM <tsuki4u@gmail.com>
2015-05-05 20:04:54 -0700

    Filter practice almost done, StatisticFilter done, begining with ParameterFilter

---
+++ practices/java-web/HelloWorldFilterWebApplication/src/main/java/com/dowa/java/web/filters/ParameterFilter.java
@@ -0,0 +1,7 @@
+package com.dowa.java.web.filters;
+
+/**
+ * Created by rabanita on 5/05/15.
+ */
+public class ParameterFilter {
+}
--- practices/java-web/HelloWorldFilterWebApplication/src/main/java/com/dowa/java/web/filters/StatisticFilter.java
+++ practices/java-web/HelloWorldFilterWebApplication/src/main/java/com/dowa/java/web/filters/StatisticFilter.java
@@ -1,12 +1,12 @@
 package com.dowa.java.web.filters;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-/**
- * Created by rabanita on 5/05/15.
- */
 public class StatisticFilter implements Filter {
+    private int visitNum = 0;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException{
         System.out.println("statisticFilter init");
@@ -17,11 +17,18 @@ public class StatisticFilter implements Filter {
             throws IOException, ServletException{
         System.out.println("statisticFilter do filter");
 
+        if (servletRequest instanceof HttpServletRequest) {
+            StringBuffer requestURL = ((HttpServletRequest) servletRequest).getRequestURL();
+            if (requestURL.toString().equals("http://localhost:9090/hello-mvc-servlet")){
+                visitNum += 1;
+            }
+        }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
     public void destroy(){
+        System.out.println("There where " + String.valueOf(visitNum) + " visits to localhost:9090/hello-mvc-servlet");
         System.out.println("statisticFilter destroy");
     }
 }
